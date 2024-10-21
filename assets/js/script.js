32b53eaedbfa91738de0e18110bca7039cec531d
Jam Samperio <jam.letsam93@gmail.com>
2024-10-21 11:47:14 -0600

    add search functionality to filter cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Get the form and the container where the recipe cards will be displayed.
+const form = document.getElementById('recipeForm');
+const recipeCardsContainer = document.getElementById('recipeCards');
+
 document.addEventListener('DOMContentLoaded', () => {
     // Get the modal and the buttons that open and close it.
     const modalBtn = document.getElementById('modalBtn');
@@ -11,10 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
     cancelBtn.addEventListener('click', () => {
         recipeModal.classList.toggle('hidden');
     });
-    
-    // Get the form and the container where the recipe cards will be displayed.
-    const form = document.getElementById('recipeForm');
-    const recipeCardsContainer = document.getElementById('recipeCards');
 
     // Waits for submit button in order to run "saveRecipe" function and dave input values.
     form.addEventListener('submit', (event) => {
@@ -23,70 +23,9 @@ document.addEventListener('DOMContentLoaded', () => {
         recipeModal.classList.toggle('hidden');
     });
 
-    // Function to save recipe in local storage
-    function saveRecipe() {
-        const title = document.getElementById('title').value;
-        const description = document.getElementById('description').value;
-        const ingredients = document.getElementById('ingredients').value;
-        const instructions = document.getElementById('instructions').value;
-
-        // Check if any fields are empty
-        if (!title || !description || !ingredients || !instructions) {
-            alert('Please fill out all sections.');
-            return;
-        }
-
-        // Object  that will be saved in local storage with unique id
-        const recipe = {
-            id: Date.now(),
-            title,
-            description,
-            ingredients,
-            instructions
-        };
-
-        // Load recipes from local storage and turn them into an array.
-        let recipes = loadRecipes();
-        recipes.push(recipe);
-        localStorage.setItem('recipes', JSON.stringify(recipes));
-        displayRecipes();
-        form.reset();
-    }
-
-    // Function to load recipes from local storage
-    function loadRecipes() {
-        const recipes = localStorage.getItem('recipes');
-        return recipes ? JSON.parse(recipes) : [];
-    }
-
-    // Function to display recipes in the DOM
-    function displayRecipes() {
-        recipeCardsContainer.innerHTML = '';
-        const recipes = loadRecipes();
-
-        recipes.forEach(recipe => {
-            const card = document.createElement('div');
-            card.classList.add('recipeCards');
-            card.innerHTML = 
-                           
-                `<h2 class="recipeTitle">${recipe.title}</h2>
-                <div>
-                    <p class="recipeDescription">${recipe.description}</p>
-                    <p class="recipeSubtitle">Ingredients</p>
-                    <ul class="recipeIngredients">
-                    ${recipe.ingredients.split(',').map(ingredient => `<li class="ingredientList">${ingredient.trim()}</li>`).join('')}
-                    </ul>
-                    <p class="recipeSubtitle">Steps</p>
-                    <p class="recipeSteps">${recipe.instructions}</p>
-                    <button onclick="deleteRecipe(${recipe.id})">Delete</button>
-                </div>`;
-            recipeCardsContainer.appendChild(card);
-        });
-    }
-
     // Function to delete recipe from local storage.
     window.deleteRecipe = function (id) {
-        let recipes = loadRecipes();
+        let recipes = getRecipes();
         recipes = recipes.filter(recipe => recipe.id !== id);
         localStorage.setItem('recipes', JSON.stringify(recipes));
         displayRecipes();
@@ -96,21 +35,89 @@ document.addEventListener('DOMContentLoaded', () => {
     displayRecipes();
 });
 
-let recipes = ['arroz con leche', 'pan de platano', 'pan frances'];
+// Function to display recipes in the DOM
+function displayRecipes(filteredRecipes) {
+    recipeCardsContainer.innerHTML = '';
+    let recipes = filteredRecipes ? filteredRecipes : getRecipes();
 
-function getInputValue() {
-  var input = document.getElementById('searchInput');
+    recipes.forEach(recipe => {
+        const card = document.createElement('div');
+        card.classList.add('recipeCards');
+        card.innerHTML =
 
-  // Obtiene el valor del input
-  var value = input.value;
+            `<h2 class="recipeTitle">${recipe.title}</h2>
+            <div>
+                <p class="recipeDescription">${recipe.description}</p>
+                <p class="recipeSubtitle">Ingredients</p>
+                <ul class="recipeIngredients">
+                ${recipe.ingredients.split(',').map(ingredient => `<li class="ingredientList">${ingredient.trim()}</li>`).join('')}
+                </ul>
+                <p class="recipeSubtitle">Steps</p>
+                <p class="recipeSteps">${recipe.instructions}</p>
+                <button onclick="deleteRecipe(${recipe.id})" class="button">Delete</button>
+            </div>`;
+        recipeCardsContainer.appendChild(card);
+    });
+}
 
-  // Muestra el valor en la consola o úsalo como prefieras
-  console.log(value);
-  let filteredRecipe = recipes.filter(val => val.includes(value))
+// Function to save recipe in local storage
+function saveRecipe() {
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const ingredients = document.getElementById('ingredients').value;
+    const instructions = document.getElementById('instructions').value;
 
-  console.log('filtro', filteredRecipe);
+    // Check if any fields are empty
+    if (!title || !description || !ingredients || !instructions) {
+        alert('Please fill out all sections.');
+        return;
+    }
+
+    // Object  that will be saved in local storage with unique id
+    const recipe = {
+        id: Date.now(),
+        title,
+        description,
+        ingredients,
+        instructions
+    };
+
+    // Load recipes from local storage and turn them into an array.
+    let recipes = getRecipes();
+    recipes.push(recipe);
+    localStorage.setItem('recipes', JSON.stringify(recipes));
+    displayRecipes();
+    form.reset();
+}
+
+function filterRecipes() {
+    // TODO: Call display recipes without filter when the input is empty
+
+    var input = document.getElementById('searchInput');
+
+    // Obtiene el valor del input
+    var value = input.value;
+
+    // Muestra el valor en la consola o úsalo como prefieras
+    console.log(value);
+    let recipes = getRecipes();
+    console.log('recipes', recipes);
+    if (recipes.length == 0) {
+        alert('No recipes found');
+        return;
+    }
+
+    let filteredRecipe = recipes.filter(recipe => recipe.title.toLowerCase().includes(value.toLowerCase()));
+    console.log('filteredRecipe', filteredRecipe);
+    displayRecipes(filteredRecipe);
 }
 
 function resetRecipes() {
-  console.log('reset', recipes);
+    console.log('reset', recipes);
+}
+
+// Function to get recipes from local storage
+function getRecipes() {
+    const recipes = localStorage.getItem('recipes');
+    return recipes ? JSON.parse(recipes) : [];
 }
